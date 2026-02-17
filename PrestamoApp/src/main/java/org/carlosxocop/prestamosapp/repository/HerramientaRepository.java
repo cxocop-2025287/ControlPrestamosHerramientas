0850083a09855577fbeaa4316ffc629616c6a477
cxocop-2025287 <cxocop-2025287@kinal.edu.gt>
2026-02-16 21:12:19 -0600

    feat: se agregó el repository de Herramienta

---
+++ PrestamoApp/src/main/java/org/carlosxocop/prestamosapp/repository/HerramientaRepository.java
@@ -0,0 +1,7 @@
+package org.carlosxocop.prestamosapp.repository;
+
+import org.carlosxocop.prestamosapp.entity.Herramienta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HerramientaRepository  extends JpaRepository<Herramienta,String> {
+}
