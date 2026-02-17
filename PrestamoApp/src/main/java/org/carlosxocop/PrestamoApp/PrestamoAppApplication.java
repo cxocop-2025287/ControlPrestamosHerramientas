7b5c31ad4cdf64f18159d849cd0fba2a80b6e7ca
cxocop-2025287 <cxocop-2025287@kinal.edu.gt>
2026-02-16 18:56:38 -0600

    feat: se agregó el proyecto base para trabajar

---
+++ PrestamoApp/src/main/java/org/carlosxocop/PrestamoApp/PrestamoAppApplication.java
@@ -0,0 +1,13 @@
+package org.carlosxocop.PrestamoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestamoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrestamoAppApplication.class, args);
+	}
+
+}
---
+++ PrestamoApp/src/test/java/org/carlosxocop/PrestamoApp/PrestamoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.carlosxocop.PrestamoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestamoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
