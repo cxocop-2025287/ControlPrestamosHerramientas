f8ff091a47f7723ec9a8fbe3e6236dbb00a86c0b
kgudiel-2025180 <kgudiel-2025180@kinal.edu.gt>
2026-02-17 16:46:34 -0600

    feat: se agregó el controlador y funciones del CRUD

--- PrestamoApp/src/main/java/org/carlosxocop/prestamosapp/controller/ClienteController.java
+++ PrestamoApp/src/main/java/org/carlosxocop/prestamosapp/controller/ClienteController.java
@@ -1,4 +1,28 @@
 package org.carlosxocop.prestamosapp.controller;
 
+
+import org.carlosxocop.prestamosapp.entity.Cliente;
+import org.carlosxocop.prestamosapp.repository.ClienteRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/clientes")
 public class ClienteController {
+    private final ClienteRepository repo;
+
+    public ClienteController(ClienteRepository repo) {this.repo = repo; }
+
+    @GetMapping
+    public List<Cliente> listar() { return repo.findAll();}
+
+    @PostMapping
+    public Cliente guardar(@RequestBody Cliente c){ return repo.save(c);}
+
+    @GetMapping("/{dpi}")
+    public Cliente buscarPorDpi(@PathVariable String dpi){
+        Cliente c = repo.findById(dpi).orElse(null);
+        return c;
+    }
 }
--- PrestamoApp/src/main/java/org/carlosxocop/prestamosapp/repository/ClienteRepository.java
+++ PrestamoApp/src/main/java/org/carlosxocop/prestamosapp/repository/ClienteRepository.java
@@ -1,4 +1,7 @@
 package org.carlosxocop.prestamosapp.repository;
 
-public interface ClienteRepository {
+import org.carlosxocop.prestamosapp.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, String> {
 }
